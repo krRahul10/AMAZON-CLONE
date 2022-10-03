0961c08ee1c3aff2e999b1f7c2501c4a6f16f5ca
Rahul <rahul10kr7@gmail.com>
2022-10-03 16:28:12 +0530

    navbar done

---
+++ Frontend/client/src/Components/header/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./navbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Avatar from "@mui/material/Avatar";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <div className="left">
+          <div className="navlog">
+            <img src="./amazon3.png" alt="" style={{ width: "8rem" }} />
+          </div>
+          <div className="nav_searchbar">
+            <input type="text" name="" id="" />
+            <div className="search_icon">
+              <SearchIcon id="search" />
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="nav_btn">
+            <a href="">Signin</a>
+          </div>
+          <div className="cart_btn">
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartIcon id="icon" />
+            </Badge>
+            <p>Cart</p>
+          </div>
+          <Avatar className="avtar" />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ Frontend/client/src/Components/newnavbaar/Newnav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './newnav.css'
+
+const Newnav = () => {
+  return (
+    <div className="new_nav">
+      <div className="nav_data">
+        <div className="left_data">
+          <p>All</p>
+          <p>Mobile</p>
+          <p>Bestseller</p>
+          <p>Fashion</p>
+          <p>Customer Service</p>
+          <p>Electronics</p>
+          <p>Today's Pay</p>
+        </div>
+        <div className="right_data">
+           <img src="./nav.jpg" alt="navdata" /> 
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Newnav;
