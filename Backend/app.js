241f2d7a8342337daf77d924c9167ca75b08e3ba
Rahul <rahul10kr7@gmail.com>
2022-10-04 17:28:19 +0530

    schema done and data send in mongo

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,25 @@
 require("dotenv").config()
 require("./db/conn")
 const express = require("express")
-const Products = require("./models/productsSchema")
-const port = 8080
 const app = express()
+const Products = require("./models/productsSchema")
+const DefaultData = require('./defaultdata')
+const cors = require("cors")
 
-app.get("/",(req,res) =>{
+const port = 8080
+
+
+
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res) =>{                           
     res.json("hello Rahul")
 })
 
 app.listen(port,()=>{
     console.log(`server start at Your PORT ${port}`)
 })
+
+DefaultData()
