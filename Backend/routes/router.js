0d6f1e9a6262a43c68dafc3007c4085e77c2ec14
Rahul <rahul10kr7@gmail.com>
2022-10-07 15:23:49 +0530

    individual routes done

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -1,19 +1,35 @@
-const express = require("express")
-const router = new express.Router()
-const Products = require("../models/productsSchema")
+const express = require("express");
+const router = new express.Router();
+const Products = require("../models/productsSchema");
 
 // *************GET PRODUCTS API *************
 
-router.get("/getproducts", async (req,res) =>{
-    try{
-        const productsdata = await Products.find()
-        res.status(201).json(productsdata)
-        // console.log("console data", productsdata)
+router.get("/getproducts", async (req, res) => {
+  try {
+    const productsdata = await Products.find();
+    res.status(201).json(productsdata);
+    // console.log("console data", productsdata)
+  } catch (err) {
+    res.status(422).json(err.message);
+    console.log("error", err.message);
+  }
+});
 
-    }catch(err){
-        console.log("error",err.message)
-    }
-})
+// ************GET INDIVIDUAL DATA *******
 
-module.exports = router
+router.get("/getproductsone/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
 
+    const individualData = await Products.findOne({ id: id });
+
+    // console.log(individualData);
+
+    res.status(201).json(individualData);
+  } catch (err) {
+    res.status(422).json(err.message);
+  }
+});
+
+module.exports = router;
